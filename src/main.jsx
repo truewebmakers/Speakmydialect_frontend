4b6fc6dafce804b35aa32567e7a68b52d9e2d1b1
AnkitaKhokhar <ankita@antiersolutions.com>
2024-06-04 20:52:00 +0530

    removed auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { App } from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import AuthProvider from "./context/authContext.jsx";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.jsx";
 import { PersistGate } from "redux-persist/integration/react";
@@ -13,9 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <App />
         <ToastContainer />
       </BrowserRouter>
     </PersistGate>
